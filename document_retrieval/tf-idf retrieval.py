5ce1b3afd12e1ad79dd1224b0c5ebf1698b4a0e5
phoenixsecularbird <49329979+phoenixsecularbird@users.noreply.github.com>
2022-10-09 15:33:57 +0800

    Create tf-idf retrieval.py

---
+++ document_retrieval/tf-idf retrieval.py
@@ -0,0 +1,288 @@
+import numpy as np
+import scipy.sparse as sp
+import unicodedata
+import copy
+import regex
+import json
+from sklearn.utils import murmurhash3_32
+
+
+STOPWORDS = {
+    'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your',
+    'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she',
+    'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their',
+    'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that',
+    'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being',
+    'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an',
+    'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of',
+    'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through',
+    'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
+    'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then',
+    'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any',
+    'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor',
+    'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can',
+    'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've',
+    'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven',
+    'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren',
+    'won', 'wouldn', "'ll", "'re", "'ve", "n't", "'s", "'d", "'m", "''", "``"
+}
+
+TFIDF_PATH = ""
+
+
+def load_sparse_csr(filename):
+    loader = np.load(filename, allow_pickle=True)
+    matrix = sp.csr_matrix((loader['data'], loader['indices'],
+                            loader['indptr']), shape=loader['shape'])
+    return matrix, loader['metadata'].item(0) if 'metadata' in loader else None
+
+
+def filter_word(text):
+    """Take out english stopwords, punctuation, and compound endings."""
+    text = unicodedata.normalize('NFD', text)
+    if regex.match(r'^\p{P}+$', text):
+        return True
+    if text.lower() in STOPWORDS:
+        return True
+    return False
+
+
+def filter_ngram(gram, mode='any'):
+    """Decide whether to keep or discard an n-gram.
+    Args:
+        gram: list of tokens (length N)
+        mode: Option to throw out ngram if
+          'any': any single token passes filter_word
+          'all': all tokens pass filter_word
+          'ends': book-ended by filterable tokens
+    """
+    filtered = [filter_word(w) for w in gram]
+    if mode == 'any':
+        return any(filtered)
+    elif mode == 'all':
+        return all(filtered)
+    elif mode == 'ends':
+        return filtered[0] or filtered[-1]
+    else:
+        raise ValueError('Invalid mode: %s' % mode)
+
+
+class Tokens(object):
+    """A class to represent a list of tokenized text."""
+    TEXT = 0
+    TEXT_WS = 1
+    SPAN = 2
+    POS = 3
+    LEMMA = 4
+    NER = 5
+
+    def __init__(self, data, annotators, opts=None):
+        self.data = data
+        self.annotators = annotators
+        self.opts = opts or {}
+
+    def __len__(self):
+        """The number of tokens."""
+        return len(self.data)
+
+    def slice(self, i=None, j=None):
+        """Return a view of the list of tokens from [i, j)."""
+        new_tokens = copy.copy(self)
+        new_tokens.data = self.data[i: j]
+        return new_tokens
+
+    def untokenize(self):
+        """Returns the original text (with whitespace reinserted)."""
+        return ''.join([t[self.TEXT_WS] for t in self.data]).strip()
+
+    def words(self, uncased=False):
+        """Returns a list of the text of each token
+        Args:
+            uncased: lower cases text
+        """
+        if uncased:
+            return [t[self.TEXT].lower() for t in self.data]
+        else:
+            return [t[self.TEXT] for t in self.data]
+
+    def offsets(self):
+        """Returns a list of [start, end) character offsets of each token."""
+        return [t[self.SPAN] for t in self.data]
+
+    def ngrams(self, n=1, uncased=False, filter_fn=None, as_strings=True):
+        """Returns a list of all ngrams from length 1 to n.
+        Args:
+            n: upper limit of ngram length
+            uncased: lower cases text
+            filter_fn: user function that takes in an ngram list and returns
+              True or False to keep or not keep the ngram
+            as_string: return the ngram as a string vs list
+        """
+        def _skip(gram):
+            if not filter_fn:
+                return False
+            return filter_fn(gram)
+
+        words = self.words(uncased)
+        ngrams = [(s, e + 1)
+                  for s in range(len(words))
+                  for e in range(s, min(s + n, len(words)))
+                  if not _skip(words[s:e + 1])]
+
+        # Concatenate into strings
+        if as_strings:
+            ngrams = ['{}'.format(' '.join(words[s:e])) for (s, e) in ngrams]
+
+        return ngrams
+
+
+class Tokenizer(object):
+    """Base tokenizer class.
+    Tokenizers implement tokenize, which should return a Tokens class.
+    """
+    def tokenize(self, text):
+        raise NotImplementedError
+
+    def shutdown(self):
+        pass
+
+    def __del__(self):
+        self.shutdown()
+
+
+class SimpleTokenizer:
+    ALPHA_NUM = r'[\p{L}\p{N}\p{M}]+'
+    NON_WS = r'[^\p{Z}\p{C}]'
+
+    def __init__(self, **kwargs):
+        """
+        Args:
+            annotators: None or empty set (only tokenizes).
+        """
+        self._regexp = regex.compile('(%s)|(%s)' % (self.ALPHA_NUM, self.NON_WS),
+                                     flags=regex.IGNORECASE + regex.UNICODE + regex.MULTILINE)
+        self.annotators = set()
+
+    def tokenize(self, text):
+        data = []
+        matches = [m for m in self._regexp.finditer(text)]
+        for i in range(len(matches)):
+            # Get text
+            token = matches[i].group()
+
+            # Get whitespace
+            span = matches[i].span()
+            start_ws = span[0]
+            if i + 1 < len(matches):
+                end_ws = matches[i + 1].span()[0]
+            else:
+                end_ws = span[1]
+
+            # Format data
+            data.append((token, text[start_ws: end_ws],span,))
+
+        return Tokens(data, self.annotators)
+
+
+class TfidfDocRanker(object):
+
+    def __init__(self, tfidf_path=None, strict=True):
+        """
+        Args:
+            tfidf_path: path to saved model file
+            strict: fail on empty queries or continue (and return empty result)
+        """
+        matrix, metadata = load_sparse_csr(tfidf_path)
+        self.doc_mat = matrix
+        self.ngrams = metadata['ngram']
+        self.hash_size = metadata['hash_size']
+        self.tokenizer = SimpleTokenizer()
+        self.doc_freqs = metadata['doc_freqs'].squeeze()
+        self.doc_dict = metadata['doc_dict']
+        self.num_docs = len(self.doc_dict[0])
+        self.strict = strict
+
+    def get_doc_index(self, doc_id):
+        """Convert doc_id --> doc_index"""
+        return self.doc_dict[0][doc_id]
+
+    def get_doc_id(self, doc_index):
+        """Convert doc_index --> doc_id"""
+        return self.doc_dict[1][doc_index]
+
+    def closest_docs(self, query, k=1):
+        """Closest docs by dot product between query and documents
+        in tfidf weighted word vector space.
+        """
+        spvec = self.text2spvec(query)
+        res = spvec * self.doc_mat
+
+        if len(res.data) <= k:
+            o_sort = np.argsort(-res.data)
+        else:
+            o = np.argpartition(-res.data, k)[0:k]
+            o_sort = o[np.argsort(-res.data[o])]
+
+        doc_scores = res.data[o_sort]
+        doc_ids = [self.get_doc_id(i) for i in res.indices[o_sort]]
+        return doc_ids
+
+    def parse(self, query):
+        """Parse the query into tokens (either ngrams or tokens)."""
+        tokens = self.tokenizer.tokenize(query)
+        return tokens.ngrams(n=self.ngrams, uncased=True, filter_fn=filter_ngram)
+
+    def hash(self, word, hash_size):
+        return murmurhash3_32(word, positive=True) % hash_size
+
+    def text2spvec(self, query):
+        """Create a sparse tfidf-weighted word vector from query.
+        tfidf = log(tf + 1) * log((N - Nt + 0.5) / (Nt + 0.5))
+        """
+        # Get hashed ngrams
+        words = self.parse(unicodedata.normalize('NFD', query))
+        wids = [self.hash(w, self.hash_size) for w in words]
+
+        if len(wids) == 0:
+            if self.strict:
+                raise RuntimeError('No valid word in: %s' % query)
+            else:
+                return sp.csr_matrix((1, self.hash_size))
+
+        # Count TF
+        wids_unique, wids_counts = np.unique(wids, return_counts=True)
+        tfs = np.log1p(wids_counts)
+
+        # Count IDF
+        Ns = self.doc_freqs[wids_unique]
+        idfs = np.log((self.num_docs - Ns + 0.5) / (Ns + 0.5))
+        idfs[idfs < 0] = 0
+
+        # TF-IDF
+        data = np.multiply(tfs, idfs)
+
+        # One row, sparse csr matrix
+        indptr = np.array([0, len(wids_unique)])
+        spvec = sp.csr_matrix((data, wids_unique, indptr), shape=(1, self.hash_size))
+
+        return spvec
+
+
+def main(dataset_file, k_tf_idf_results=10):
+
+    ranker = TfidfDocRanker(TFIDF_PATH)
+
+    with open(dataset_file) as f:
+        dataset_list = [json.loads(line) for line in f]
+
+    for item in dataset_list:
+        item["tfidf_docs"] = ranker.closest_docs(item["claim"], k=k_tf_idf_results)
+
+    return dataset_list
+
+
+if __name__ == "__main__":
+
+    dataset_file = ""
+    k_tf_idf_results = 10
+    main(dataset_file, k_tf_idf_results)
